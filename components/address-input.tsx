3522f2e051da1f195cea371f5924f5c158e47bb3
Satyam Kulkarni <punekar.satyam@gmail.com>
2024-01-21 17:49:05 +0530

    feat: added request address

--- components/address-input.tsx
+++ components/address-input.tsx
@@ -104,8 +104,9 @@ const AddressInput = () => {
       <div className="relative mx-auto flex h-16 w-full max-w-[699px] items-center rounded-full border border-primary p-8 shadow-[inset_0px_1px_4px_1px_rgba(0,0,0,0.25)]">
         <input
           type="text"
-          placeholder="Request address"
+          placeholder="Your address"
           spellCheck={false}
+          value={walletAddress}
           onChange={(e) => setWalletAddress(e.target.value)}
           className="placeholder:text-muted-foreground block w-full bg-transparent pr-24 text-lg ring-0 placeholder:text-[#777777] focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:pr-36"
         />
